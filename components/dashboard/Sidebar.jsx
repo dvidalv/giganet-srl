c0695d750ebd8683c947a38f58e9ed8941dca660
David Vidal <dvidalv@gmail.com>
2026-02-24 15:57:41 -0400

    Update header logo dimensions and enhance sidebar navigation icons
    Adjust the logo size in the header for better responsiveness on smaller screens, changing dimensions to 100x50 pixels. Update the sidebar navigation to use React icons for improved visual clarity and consistency across user roles, enhancing the overall user experience.

--- components/dashboard/Sidebar.jsx
+++ components/dashboard/Sidebar.jsx
@@ -2,33 +2,40 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./Sidebar.module.css";
+import { RiHome9Fill } from "react-icons/ri";
+import { ImQrcode } from "react-icons/im";
+import { BsBuildings } from "react-icons/bs";
+import { IoPeopleCircle } from "react-icons/io5";
+import { FcDataConfiguration } from "react-icons/fc";
+import { IoKeySharp } from "react-icons/io5";
+import { IoBarChartSharp } from "react-icons/io5";
 
 export default function Sidebar({ user }) {
   const { name, email, role } = user;
   const pathname = usePathname();
   
   const navItems = [
-    { label: "Dashboard", href: "/dashboard", icon: "🏠" },
+    { label: "Dashboard", href: "/dashboard", icon: <RiHome9Fill /> },
     ...(role === "admin"
       ? [
-          { label: "Usuarios", href: "/dashboard/usuarios", icon: "👥" },
-          { label: "Empresas", href: "/dashboard/empresas", icon: "🏢" },
+          { label: "Usuarios", href: "/dashboard/usuarios", icon: <IoPeopleCircle /> },
+          { label: "Empresas", href: "/dashboard/empresas", icon: <BsBuildings /> },
         ]
       : []),
     ...(role === "admin"
-      ? [{ label: "Comprobantes", href: "/dashboard/comprobantes", icon: "🔳" }]
+      ? [{ label: "Comprobantes", href: "/dashboard/comprobantes", icon: <ImQrcode /> }]
       : []),
     ...(role !== "admin"
-      ? [{ label: "Mis comprobantes", href: "/dashboard/mis-comprobantes", icon: "🔳" }]
+      ? [{ label: "Mis comprobantes", href: "/dashboard/mis-comprobantes", icon: <ImQrcode /> }]
       : []),
     ...(role !== "admin"
-      ? [{ label: "Mi Empresa", href: "/dashboard/empresa", icon: "🏢" }]
+      ? [{ label: "Mi Empresa", href: "/dashboard/empresa", icon: <BsBuildings /> }]
       : []),
     ...(role !== "admin"
-      ? [{ label: "API Key", href: "/dashboard/api-key", icon: "🔑" }]
+      ? [{ label: "API Key", href: "/dashboard/api-key", icon: <IoKeySharp /> }]
       : []),
-    { label: "Reportes", href: "/dashboard/reportes", icon: "📊" },
-    { label: "Configuración", href: "/dashboard/configuracion", icon: "⚙️" },
+    { label: "Reportes", href: "/dashboard/reportes", icon: <IoBarChartSharp /> },
+    { label: "Configuración", href: "/dashboard/configuracion", icon: <FcDataConfiguration /> },
   ];
 
   return (
